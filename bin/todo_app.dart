2fc0af374ca60013f9133cd672e3745b9861c662
duxa4854 <nzaichenko531@gmail.com>
2026-04-08 11:32:19 +0300

    initial commit

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,31 +1,37 @@
+import 'dart:io';
+import 'package:todo_app/todo.dart';
+import 'package:todo_app/todo_repository.dart';
+
 void main() {
-  print('Hello world!');
-  String name = 'Федоров';
-  String? name2 = null;
-  var count = 0;
-  var title = "учить раст";
-  var x = 5;
-  // x = "hello";
-  final id;
-  id = 5;
-  const appName = "TodoApp";
-  print('$name, $name2, $count,$title,$id,$x');
-  List<String> tags = ['институт','дом'];
-  Map<String, dynamic> data = {'key':'value', 'num': 10};
-  Set<int> numbers = {1,2,3,1};
-  String greet(String name) => 'Hello $name';
-  printTodo(title: "купить пожрать");
-  printTodo(title: 'сделать дз', done: true);
-}
-void printTodo({required String title, bool done = false}){
-  print('${done ? '+' : '-'} $title');
+  TodoRepository todo = TodoRepository();
+  printMenu();
+  while (true) {
+    stdout.write('> ');
+    String? input = stdin.readLineSync();
+    if (input == null) {
+      continue;
+    }
+    input = input.trim();
+    if (input.isEmpty) {
+      continue;
+    }
+    bool shouldExit = handleCommand(repo, input);
+    if(shouldExit) {
+      break;
+    }
+  }
 }
 
-class Todo {
-  final int id;
-  String title;
-  bool isDone;
-  Todo({required this.id, required this.title, this.isDone = false});
+bool handleCommand(repo, String input) {
+}
 
-  
+void printMenu() {
+  print('Приложение TODO');
+  print('Команды');
+  print(' add <текст>   - добавит задачу');
+  print(' list   - показать список');
+  print(' done<id>   - отметить выполненной');
+  print(' delete<id>   - удалить задачу');
+  print(' exit   - выход');
+
 }
